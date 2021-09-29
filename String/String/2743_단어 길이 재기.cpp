f43e7c6c26eec49fea90efe502c182a5e583c7a2
HyungJin <hyungjin0710@gmail.com>
2021-09-29 17:06:00 +0900

    add 2743_단어 길이 재기.cpp

---
+++ String/String/2743_단어 길이 재기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+#define endl '\n'
+
+using namespace std;
+
+void soulution() {
+	string s;
+	cin >> s;
+	cout << s.length() << endl;
+}
+
+void solve() {
+	soulution();
+}
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	soulution();
+
+	return 0;
+}
