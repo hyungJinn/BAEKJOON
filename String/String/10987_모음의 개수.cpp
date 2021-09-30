cd7066d3b4bfa3ec17aea07a6314dec9165c7ec7
HyungJin <hyungjin0710@gmail.com>
2021-09-30 19:11:49 +0900

    Update 10987_모음의 개수.cpp Use "find()".

--- String/String/10987_모음의 개수.cpp
+++ String/String/10987_모음의 개수.cpp
@@ -7,13 +7,21 @@
 using namespace std;
 
 int solution() {
-	int result = 0;
+	int result = 0, idx = 0;
 	string str = "";
+	vector<string> vowel = { "a", "e", "i", "o", "u" };
 
 	cin >> str;
-	for (int i = 0; i < str.size(); i++) {
-		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+	for (int i = 0; i < vowel.size();) {
+		idx = str.find(vowel[i], idx);
+		if (idx == -1) {
+			i++;
+			idx = 0;
+		}
+		else {
 			result++;
+			idx = idx + 1;
+		}
 	}
 	return result;
 }
