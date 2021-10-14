3b1b7967827cf0e694c63e07fc8b4c6a11baf84c
HyungJin <hyungjin0710@gmail.com>
2021-10-14 19:38:22 +0900

    add 10171_고양이.cpp

---
+++ Implementation/Implementation/10171_고양이.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+void solution() {
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+}
+
+void solve() {
+    solution();
+}
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    solve();
+
+    return 0;
+}
