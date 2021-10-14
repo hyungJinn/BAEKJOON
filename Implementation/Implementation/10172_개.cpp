b93265589dace55a86be695c3ab6b2b0b7c5dbe4
HyungJin <hyungjin0710@gmail.com>
2021-10-14 19:38:37 +0900

    add 10172_개.cpp

---
+++ Implementation/Implementation/10172_개.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+void solution() {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_ /= \\__ |" << endl;
+}
+
+void solve() {
+	solution();
+}
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	solve();
+
+	return 0;
+}
